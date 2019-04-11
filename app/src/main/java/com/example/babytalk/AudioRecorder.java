dccd461d0b51538eb8436d63c8d450653b9dafb5
frume02 <frume02@yahoo.de>
2019-04-11 13:34:36 +0200

    Switched to AudioRecorder first draft

---
+++ app/src/main/java/com/example/babytalk/AudioRecorder.java
@@ -0,0 +1,96 @@
+package com.example.babytalk;
+
+import android.media.AudioFormat;
+import android.media.AudioManager;
+import android.media.AudioRecord;
+import android.media.AudioTrack;
+import android.media.MediaRecorder;
+import android.util.Log;
+
+/*
+     * Thread to manage live recording/playback of voice input from the device's microphone.
+     * Source adapted from https://stackoverflow.com/questions/6959930/android-need-to-record-mic-input
+     */
+    public class AudioRecorder extends Thread
+    {
+        private boolean stopped = false;
+        private AudioRecord recorder = null;
+        private AudioTrack track = null;
+        private short[][]   buffers  = new short[256][160];
+        private int ix = 0;
+        private int maxAmplitude=0;
+
+        /**
+         * Give the thread high priority so that it's not canceled unexpectedly, and start it
+         */
+        protected AudioRecorder()
+        {
+            android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_URGENT_AUDIO);
+        }
+
+        @Override
+        public void run()
+        {
+            Log.i("Audio", "Running Audio Thread");
+
+            /*
+             * Initialize buffer to hold continuously recorded audio data, start recording, and start
+             * playback.
+             */
+            try
+            {
+                int N = AudioRecord.getMinBufferSize(8000,AudioFormat.CHANNEL_IN_MONO,AudioFormat.ENCODING_PCM_16BIT);
+                recorder = new AudioRecord(MediaRecorder.AudioSource.MIC, 8000, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, N*10);
+                track = new AudioTrack(AudioManager.STREAM_MUSIC, 8000,
+                        AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_16BIT, N*10, AudioTrack.MODE_STREAM);
+                recorder.startRecording();
+                track.play();
+
+                /*
+                 * Loops until something outside of this thread stops it.
+                 * Reads the data from the recorder and writes it to the audio track for playback.
+                 */
+                while(!stopped)
+                {
+                    Log.i("Map", "Writing new data to buffer");
+                    short[] buffer = buffers[ix++ % buffers.length];
+                    N = recorder.read(buffer,0,buffer.length);
+                    for (short s : buffer)
+                    {
+                        if (Math.abs(s) > maxAmplitude)
+                        {
+                            maxAmplitude = Math.abs(s);
+                        }
+                    }
+                    track.write(buffer, 0, buffer.length);
+                }
+            }
+            catch(Throwable x)
+            {
+                Log.w("Audio", "Error reading voice audio", x);
+            }
+            /*
+             * Frees the thread's resources after the loop completes so that it can be run again
+             */
+            finally
+            {
+                recorder.stop();
+                recorder.release();
+                track.stop();
+                track.release();
+            }
+        }
+
+        /**
+         * Called from outside of the thread in order to stop the recording/playback loop
+         */
+        protected void close()
+        {
+            stopped = true;
+        }
+
+        public double getMaxAmplitude() {
+            return maxAmplitude;
+        }
+
+    }
--- app/src/main/java/com/example/babytalk/MonitorService.java
+++ app/src/main/java/com/example/babytalk/MonitorService.java
@@ -1,53 +1,31 @@
 package com.example.babytalk;
 
-import android.Manifest;
 import android.app.Service;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.media.AudioFormat;
-import android.media.AudioRecord;
-import android.media.MediaPlayer;
-import android.media.MediaRecorder;
-import android.os.Bundle;
 import android.os.IBinder;
-import android.support.v4.app.ActivityCompat;
 import android.util.Log;
-
-import java.io.IOException;
 
 public class MonitorService extends Service {
     private static final String LOG_TAG = "MonitorService";
-    private static String fileName = null;
 
     private boolean isRunning = false;
+    private AudioRecorder audioRecorder = null;
 
-    private MediaRecorder recorder = null;
-    private MediaPlayer player = null;
-
-    private int minSize;
 
     public int onStartCommand(Intent intent, int flags, int startId) {
 
         Log.i(LOG_TAG, "Service onStart");
+        audioRecorder=new AudioRecorder();
+        audioRecorder.start();
 
-        // Record to the external cache directory for visibility
-        fileName = getExternalCacheDir().getAbsolutePath();
-        fileName += "/audiorecordtest.3gp";
-
-
-       // minSize= AudioRecord.getMinBufferSize(8000, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
         isRunning = true;
         new Thread(new Runnable() {
             public void run() {
                 Log.i(LOG_TAG, "Service running");
                 while (isRunning){
                     try {
-                        startRecording();
                         Thread.sleep(1000);
-                        Log.i(LOG_TAG, "Current amplitude " + getAmplitude());
-                        stopRecording();
-                        Thread.sleep(3000);
-
+                        Log.i(LOG_TAG, "Current maximum amplitude " + audioRecorder.getMaxAmplitude());
                     } catch (InterruptedException e) {
                         Log.i(LOG_TAG, "Thread InterruptedException");
                     }
@@ -65,68 +43,8 @@ public class MonitorService extends Service {
 
     public void onDestroy() {
         isRunning = false;
-        if (recorder != null) {
-            recorder.release();
-            recorder = null;
-        }
-        if (player != null) {
-            player.release();
-            player = null;
-        }
-
+        audioRecorder.close();
         Log.i(LOG_TAG, "Service destroyed");
-    }
-
-    private void startRecording() {
-        recorder = new MediaRecorder();
-        recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-        recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-        recorder.setOutputFile(fileName);
-        recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-
-        try {
-            recorder.prepare();
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "prepare() failed");
-        }
-
-        recorder.start();
-    }
-
-    private void stopRecording() {
-        recorder.stop();
-        recorder.release();
-        recorder = null;
-    }
-
-
-    public double getAmplitude() {
-       /* short[] buffer = new short[minSize];
-        ar.startRecording();
-        //Thread.sleep(20);
-        // TODO Set a delay for recording
-        ar.read(buffer, 0, minSize);
-        ar.stop();
-        int max = 0;
-        for (short s : buffer)
-        {
-            if (Math.abs(s) > max)
-            {
-                max = Math.abs(s);
-            }
-        }*/
-        /*player = new MediaPlayer();
-        try {
-            player.setDataSource(fileName);
-            player.prepare();
-            player.start();
-
-        } catch (IOException e) {
-            Log.e(LOG_TAG, "prepare() failed");
-        }*/
-        // TODO get maximum amplitude
-        return recorder.getMaxAmplitude();
-        //return player.getDuration();
     }
 }
 
